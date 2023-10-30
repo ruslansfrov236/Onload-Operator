e4441ff13544c8699e9d539256d5fc6e449f2e02
Ruslan Seferov <91841957+ruslansfrov236@users.noreply.github.com>
2023-10-30 21:14:36 +0400

    Add files via upload

---
+++ OverLoading/OverLoading/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+Database database = new Database();
+bool connectionState = database + DatabaseType.PostgreSql;
+Console.WriteLine(connectionState);
+
+public class Database
+{
+  public static string _connectionString;
+
+    public static bool operator +(Database database, DatabaseType databaseType)
+    {
+        _connectionString = databaseType switch
+        {
+            DatabaseType.SqlServer => "Data Source= yourServerAddress;Initial Catalog= yourDatabaseName;User ID= yourUsername;Password= yourPassword",
+            DatabaseType.PostgreSql=> "Host = yourServerAddress;Port= 5432;Database= yourDatabaseName;Username= yourUsername;Password= yourPassword;",
+            DatabaseType.Oracle=> "User Id=yourUsername;Password=yourPassword;Data Source=yourTnsOrConnectionString;",
+            _ => throw new ArgumentException("Invalid database type"),
+        };
+
+
+        return true;
+
+
+    }
+}
+
+enum DatabaseType
+{
+    SqlServer, Oracle, PostgreSql
+
+}
+
+
+
+
